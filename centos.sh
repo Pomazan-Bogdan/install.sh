b63e46b9e91801ac069ecdb0f60e30d14ea2da94
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2019-12-27 01:07:32 +0200

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -2,5 +2,4 @@
 wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
 wget -q -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 git clone git://github.com/inishchith/autoenv.git ~/.autoenv
-sudo echo '/usr/bin/zsh' >> /etc/shells
 chsh -s $(which zsh)
