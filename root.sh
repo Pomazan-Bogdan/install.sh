12eeb5131716d10240fb6ec044d7376f302ecd43
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2019-12-27 01:08:44 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -9,6 +9,7 @@ cp /root/.ssh/authorized_keys /home/centos/.ssh/authorized_keys
 chown -R centos:centos /home/centos/.ssh
 echo 'Port 22222' >> /etc/ssh/sshd_config
 echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+echo '/usr/bin/zsh' >> /etc/shells
 yum update -y
 yum install git mc htop wget zsh chrony screen -y
 systemctl enable chronyd & systemctl start chronyd & shutdown -r now 
