245e1bc20a1f73d4585398c613633cdbc39a3662
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2019-12-27 00:58:53 +0200

    Update and rename install2.sh to centos.sh

--- centos.sh
+++ centos.sh
@@ -2,5 +2,5 @@
 wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
 wget -q -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 git clone git://github.com/inishchith/autoenv.git ~/.autoenv
-echo '/usr/bin/zsh' >> /etc/shells
+sudo echo '/usr/bin/zsh' >> /etc/shells
 chsh -s $(which zsh)
