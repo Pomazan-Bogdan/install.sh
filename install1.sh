8c36e0f6e04605a89ddda0533e0588366c0046e4
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2019-12-27 00:37:05 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
-adduser centos \
-passwd centos \
-usermod -aG wheel centos \
-passwd -d centos \
+adduser centos & \
+passwd centos & \
+usermod -aG wheel centos & \
+passwd -d centos & \
+echo 'centos ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers & \
+mkdir /home/centos/.ssh & \
+cp /root/.ssh/authorized_keys /home/centos/.ssh/authorized_keys & \
+chown -R centos:centos /home/centos/.ssh & \
+echo 'Port 22222' >> /etc/ssh/sshd_config & \
+echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config & \
 yum update -y
 yum install git mc htop wget zsh chrony screen -y
-systemctl enable chronyd && systemctl start chronyd && chronyc sources 
+systemctl enable chronyd & systemctl start chronyd & shutdown -r now 
