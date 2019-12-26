8b99d5937a5de0aed94b6c5daabac47bf02d5ff4
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2019-12-27 00:18:05 +0200

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
+wget -q -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+git clone git://github.com/inishchith/autoenv.git ~/.autoenv
+echo '/usr/bin/zsh' >> /etc/shells
+chsh -s $(which zsh)
