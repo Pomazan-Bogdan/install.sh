170db8fd7adb6278e602e21cc146edde0129ab6e
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2019-12-26 17:41:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo systemctl enable chronyd && sudo systemctl start chronyd && chronyc sources
 curl https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
 wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash &&
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-$ git clone git://github.com/inishchith/autoenv.git ~/.autoenv
+git clone git://github.com/inishchith/autoenv.git ~/.autoenv
 rm .zshrc
 wget https://raw.githubusercontent.com/Pomazan-Bogdan/install.sh/master/.zshrc
 echo '/usr/bin/zsh' >> /etc/shells
