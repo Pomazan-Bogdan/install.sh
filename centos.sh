3a937db0504f1361595b6e2da1300e02c26ffd2e
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2020-04-16 11:19:18 +0300

    del

--- centos.sh
+++ centos.sh
@@ -3,4 +3,4 @@ wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script
 wget -q -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 git clone git://github.com/inishchith/autoenv.git ~/.autoenv
 chsh -s /usr/bin/zsh
-sudo dnf config-manager --setopt=fastestmirror=True --save
+
