6336ee5044ac02312b7e43f30240380b0f8cd4a2
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2020-05-02 12:25:57 +0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf makecache
+sudo dnf install --nobest docker-ce
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
