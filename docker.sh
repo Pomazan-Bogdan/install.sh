b1e2bc666313e1f74795bd9194d59f3a0b2b3ad2
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2020-05-02 13:20:53 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,5 +12,6 @@ sudo docker plugin install grafana/loki-docker-driver:latest --alias loki --gran
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
