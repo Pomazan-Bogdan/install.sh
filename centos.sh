a8e8666558d337aaf594f384459a84a6b67c94b5
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2020-05-02 12:58:50 +0300

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -2,7 +2,7 @@
 wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
 wget -q -O - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 git clone git://github.com/inishchith/autoenv.git ~/.autoenv
-sed -i 's/.*ШАБЛОН.*/plugins=(git dnf kubectl zsh_reload go golang git helm postgres autoenv colorize cp docker docker-compose pip screen sudo systemd)/' /home/centos/.zshrc
+sed -i 's/.*plugins=(git).*/plugins=(git dnf kubectl zsh_reload go golang git helm postgres autoenv colorize cp docker docker-compose pip screen sudo systemd)/' /home/centos/.zshrc
 
 chsh -s /usr/bin/zsh
 
