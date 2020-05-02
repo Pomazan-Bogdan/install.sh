fccc31276a57d98067aad1eee8944d7ec3ff00bb
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2020-05-02 12:37:07 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -5,6 +5,7 @@ sudo dnf makecache
 dnfi -y --nobest docker-ce
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+sudo usermod -aG docker $USER
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
