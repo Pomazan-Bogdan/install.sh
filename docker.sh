3cc10a04569378287a5c3f7eedcc9c84989042aa
Pomazan Bogdan <pomazan.bogdan@gmail.com>
2020-05-02 12:51:22 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,6 +6,6 @@ dnfi -y --nobest docker-ce
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo usermod -aG docker $USER
-
+sudo docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
